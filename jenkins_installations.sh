f5be49f22d2d7baa7900a8b25cd225e9decf23e2
naresh <nrsgit@gmail.com>
2025-07-24 13:30:14 +0530

    jenkins_installations

---
+++ jenkins_installations.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk
+java --version
+sleep 5
+sudo apt-get install -y git
+git --version
+sleep 5
+sudo apt-get install -y maven
+maven --version
+sleep 5
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y jenkins
+jenkins --version
+sleep 5
+echo "jenkins installation is successful"
+sleep 5
+
